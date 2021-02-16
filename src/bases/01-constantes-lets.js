f368eb70ec0446174d833a47e92b17f91f53b752
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-16 13:17:39 -0500

    #2- Variables y lets (el var ya no se usa)

--- src/bases/01-constantes-lets.js
+++ src/bases/01-constantes-lets.js
@@ -1,10 +1,12 @@
+ 
+ // constantes y variables
  const nombre = 'Henrry';
  let apellido = 'Orbezo';
-
  let valorDado = 18;
  valorDado = 17;
  console.log(nombre ,apellido , valorDado);
 //variables de scope
+//en java script nose usa el var
  if (true) {
      const nombre = 'Peter';
    //  let valorDado = 20;
