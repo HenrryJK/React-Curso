86286ea80a750e13bb16967c7177e6c47be720ce
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-16 17:13:33 -0500

    #5 Arreglos y uso de objetos arrats

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
-const person = {
-    first_name : 'Henrry' , 
-    last_name : 'Orbezo' , 
-    ages: 18,
-    address: {
-        city : 'Lima' ,
-        URL: 'Av.Masmorras' ,
-        Mz: 35 , 
-        Lote: '13 A'
-    }
-};
 
-// console.log({ person:person });
-// console.log({ person });
 
-console.log( person );
- // console.table({ person });
-
-// const person2 = person;
-const person2 = {...person};
-person2.first_name = 'Santiago';
-
-console.log(person2);
