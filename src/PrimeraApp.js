abdfd5ad23b832787dbbeb35253d3d66a1b4fb81
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-18 16:33:20 -0500

    Impresión de variables en el HTML #4

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,11 +2,23 @@ import React, { Fragment } from 'react';
 
 // Functional Components
 const PrimeraApp = () => { 
+    const message = 'Hurry Up! Country Alemania!..';
+    const poblations = 1262.32000;
+    const numbers = [1 , 2 , 3,4,5,6]
+
+    const persona ={
+        first_name : 'Henrry' , 
+        ages : 18
+    }
     return (
         // <div>   </div>   // <>  </> 
         <Fragment> 
-        <h1>Hello world !!! How are you??</h1>
-    <p>Welcome people!! One more time at party California!!</p>
+    <h1>{message}</h1>
+    <p>Welcome people!! One more time at party Berlín!!</p>
+    <p>Con {poblations} de Habitantes...!</p>
+        <h3>{numbers}</h3>
+    <h4> Autor : {JSON.stringify(persona)}</h4>
+    <pre> Autor : {JSON.stringify(persona , null , 1)}</pre>
         </Fragment>
        ) ;
 }
