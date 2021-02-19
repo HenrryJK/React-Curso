5be8a90dc72312904a801480b91aca2ff3023800
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-19 03:34:53 -0500

    Evento click (Eventos en general) #9

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,10 +3,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({values}) => {
+    //handeAdd
+    const handleAdd = (e) => {
+        console.log(e);
+    }
+    const FollowResponse = (e) => {
+       return () => console.log('Hello World!!')
+    }
+
     return (
         <>
            < h1>CounterApp</h1>
             <h2> { values } </h2>
+        <button onClick={(e) =>{handleAdd(e)}}>+1</button>
+
+        <button onClick={handleAdd}>I like !</button>
+        <button onClick={FollowResponse()}>Join Us !</button>
         </>
     )
 }
